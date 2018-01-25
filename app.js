93ceeb95b75c6a1f8fabe73079e2e58f92883b30
caime.daniel <caime.daniel@outlook.com>
2018-01-25 17:00:14 -0300

    srvproxy

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 
  //modules
 var express = require('express');
+const bodyParser = require('body-parser')
 //routes
 var index = require('./routes/index');
 var search = require('./routes/search');
@@ -12,16 +13,42 @@ var webhooks = require('./routes/webhooks');
 //
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// parse application/json
+app.use(bodyParser.json())
+
 //views
 app.set('view engine', 'ejs');
 
 //midleware ?
-//app.use(express.static(__dirname));
+app.use(express.static(__dirname));
 //paths
 app.use('/',index);
 app.use('/search',search);
 app.use('/webhook',webhooks);
 
+/**
+ * errors
+ */
+
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+  
+  app.use(function(err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
 //listen
 app.listen(app.get('port'),()=>{
     console.log('node app is listen in');
--- atgservice/atgserviceproxy.js
+++ atgservice/atgserviceproxy.js
@@ -7,6 +7,7 @@ var searchsrv = function(param){
     const resperpage = 10;
     const codsucursal = 200;
     const options = {
+        uri:     `http://www.cotodigital3.com.ar/AtgServiceWS/restfull/searchws/search/200?textinput=${param}&cantidadPorPagina=10`,
         hostname: 'http://www.cotodigital3.com.ar',
         port: null,
         path: `/AtgServiceWS/restfull/searchws/search/${codsucursal}?textinput=${param}&cantidadPorPagina=${resperpage}`,
@@ -16,9 +17,9 @@ var searchsrv = function(param){
     return new Promise((resolve, reject) => {
         
         //var uri       = "http://localhost:3000/search";
-        var uri       = "http://www.cotodigital3.com.ar/AtgServiceWS/restfull/searchws/search/200?textinput=smart&cantidadPorPagina=10";
+        //var uri       = "http://www.cotodigital3.com.ar/AtgServiceWS/restfull/searchws/search/200?textinput=smart&cantidadPorPagina=10";
         
-        request.get(uri, function (error, response, body) {
+        request.get(options, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log(param);
                 resolve(body);
