83025866751d4d68c6df70a78004ead12a1effbc
caime.daniel <caime.daniel@outlook.com>
2018-01-25 12:29:51 -0300

    onError

--- atgservice/atgserviceproxy.js
+++ atgservice/atgserviceproxy.js
@@ -15,10 +15,10 @@ var searchsrv = function(param){
 
     return new Promise((resolve, reject) => {
         
-        var uri       = "http://localhost:3000/search";
-        //var uri       = "http://www.cotodigital3.com.ar/AtgServiceWS/restfull/searchws/search/200?textinput=smart&cantidadPorPagina=10";
+        //var uri       = "http://localhost:3000/search";
+        var uri       = "http://www.cotodigital3.com.ar/AtgServiceWS/restfull/searchws/search/200?textinput=smart&cantidadPorPagina=10";
         
-        request.get(options, function (error, response, body) {
+        request.get(uri, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log(param);
                 resolve(body);
